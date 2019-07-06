dc566644731435e038daf91f2649007290918019
BeautyCIT <52609460+BeautyCIT@users.noreply.github.com>
2019-07-06 23:17:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ client.login('NTk3MTQ2NzI4NTc1MDA4NzY4.XSD3Dg.pgjkCjIZNWQlVpwp5vzygzl0dQs')
 var http = require("http");
 setInterval(function() {
     http.get("http://<your app name>.herokuapp.com");
-}, 60000); // every 5 minutes (300000)
+}, 300000); // every 5 minutes (300000)
