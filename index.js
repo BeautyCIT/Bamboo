83c4415d699ea37b41790aa4f33783e9240fd4a3
BeautyCIT <52609460+BeautyCIT@users.noreply.github.com>
2019-07-06 23:53:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 })
 
 client.on('message', msg => {
-  if (msg.content.includes === 'ping') {
-    msg.reply('Pong!')
+  if (message.content.includes === 'ping') {
+    message.reply('Pong!')
   }
 })
 
